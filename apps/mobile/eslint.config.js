fb52de1a3306b1298c3580fa60b56f7abed3785b
nguyenhungAAA <quanghunglenguyen@gmail.com>
2026-09-14 14:28:16 +0700

    style: change work panel style desgin, add initial testing

--- apps/mobile/eslint.config.js
+++ apps/mobile/eslint.config.js
@@ -1,10 +1,19 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require("eslint-config-expo/flat");
+const testingLibrary = require('eslint-plugin-testing-library');
 
 module.exports = defineConfig([
   expoConfig,
   {
     ignores: ["dist/*"],
-  }
+  },
+  {
+    files: [
+      '**/__tests__/**/*.[jt]s?(x)',
+      '**/?(*.)+(spec|test).[jt]s?(x)',
+    ],
+    ...testingLibrary.configs['flat/react'],
+  },
+
 ]);
